faa987595c31a9a4874271bc1b9e4790a6004a44
Wanderson <you@example.com>
2023-08-21 10:08:02 -0300

    Criação dos componentes e estrutura inicial

---
+++ todo/todo/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TaskList } from './TaskList'
+
+
+export function Main(){
+    return(
+        <>
+            <TaskList/>
+        </>
+    )
+}
---
+++ todo/todo/src/App.tsx
@@ -0,0 +1,17 @@
+import {Header} from '../components/Header'
+import {Search} from '../components/Search'
+import {Main} from '../components/Main'
+import './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/> 
+      <Search />
+      <Main/>
+    </>
+  )
+}
+
+export default App
