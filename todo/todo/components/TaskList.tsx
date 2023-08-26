869433da9e10bebed372ce24459260fa2b4cc27e
Nascimento W <wandersonnascimento94@gmail.com>
2023-08-26 19:02:59 -0300

    Realocação de elementos fora da task list

--- todo/todo/components/TaskList.tsx
+++ todo/todo/components/TaskList.tsx
@@ -3,50 +3,27 @@ import styles from './TaskList.module.css'
 import clipboard from '../src/assets/Clipboard.png'
 import trash from '../src/assets/trash.svg'
 
-export function TaskList() {
+interface listaDeTarefasProps {
+    key: number,
+    isFinished: boolean,
+    text: string,
+
+}
+
+
+export function TaskList({ key, isFinished, text }: listaDeTarefasProps) {
     return (
         <div className={styles.tasklist}>
-            <header>
-                <div className={styles.headerelements}>
-                    <p className={styles.azul}>Tarefas criardas</p> <span>0</span>
-                </div>
-                <div className={styles.headerelements}>
-                    <p className={styles.roxo}>Concluídas</p> <span>0</span>
-                </div>
-            </header>
             <main>
                 <div className={styles.element}>
                     <label>
                         <input type="checkbox" />
                         <span>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                            {text}
                         </span>
                     </label>
                     <img src={trash} alt="icone de lixo" />
                 </div>
-                <div className={styles.element}>
-                    <label>
-                        <input type="checkbox" />
-                        <span>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        </span>
-                    </label>
-                    <img src={trash} alt="icone de lixo" />
-                </div>
-                <div className={styles.element}>
-                    <label>
-                        <input type="checkbox" />
-                        <span>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        </span>
-                    </label>
-                    <img src={trash} alt="icone de lixo" />
-                </div>
-                <img
-                    src={clipboard} alt="icone do clipboard"
-                />
-                <strong>Você ainda não tem tarefas cadastradas</strong>
-                <p>Crie tarefas e organize seus itens a fazer</p>
             </main>
         </div>
     )
--- todo/todo/src/App.tsx
+++ todo/todo/src/App.tsx
@@ -1,9 +1,25 @@
 import { Header } from '../components/Header'
 import { Search } from '../components/Search'
-import { Main } from '../components/Main'
-
+import { TaskList } from '../components/TaskList'
+import clipboard from '../src/assets/Clipboard.png'
 import styles from './App.module.css'
+
 import './global.css'
+
+const listaDeTarefas = [
+  {
+    id: 1,
+    isFinished: false,
+    text: 'Wanderson Nascimento turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 1,
+    isFinished: false,
+    text: 'Nayara loiuse Floriano Nascimento libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+
+  }
+]
+
 
 function App() {
 
@@ -12,7 +28,34 @@ function App() {
       <Header />
       <div>
         <Search />
-        <Main />
+        <main>
+          <header>
+            <div className={styles.headerelements}>
+              <p className={styles.azul}>Tarefas criardas</p> <span>0</span>
+            </div>
+            <div className={styles.headerelements}>
+              <p className={styles.roxo}>Concluídas</p> <span>0</span>
+            </div>
+          </header>
+          {
+            listaDeTarefas.map(tarefa => {
+              return (
+                <TaskList
+                  key={tarefa.id}
+                  isFinished={tarefa.isFinished}
+                  text={tarefa.text}
+                />
+              )
+            })
+          }
+        </main>
+        <div>
+          <img
+            src={clipboard} alt="icone do clipboard"
+          />
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
       </div>
     </>
   )
