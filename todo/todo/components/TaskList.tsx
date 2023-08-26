caf45feaff8bbbf19dde0f8ba4c7624c1a032964
Nascimento W <wandersonnascimento94@gmail.com>
2023-08-26 12:34:43 -0300

    CSS da tela sem task finalizado

--- todo/todo/components/TaskList.tsx
+++ todo/todo/components/TaskList.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from './TaskList.module.css'
+import clipboard from '../src/assets/Clipboard.png'
 
 export function TaskList(){
     return(
-        <article className={styles.tasklist}>
-            <header className={styles.header}>
+        <div className={styles.tasklist}>
+            <header>
                 <p>Tarefas criardas 0</p>
                 <p>Concluídas 0</p>
             </header>
             <main>
-                <p>Icone</p>
+                <img
+                    src={clipboard} alt="icone do clipboard"
+                />
                 <strong>Você ainda não tem tarefas cadastradas</strong>
                 <p>Crie tarefas e organize seus itens a fazer</p>
             </main>
-        </article>
+        </div>
     )
 }
