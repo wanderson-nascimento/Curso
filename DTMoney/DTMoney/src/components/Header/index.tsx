6d944db72da9c9a806a9dc554d2be5df4adcad08
unknown <wandersonnascimento94@gmail.com>
2023-11-06 14:51:37 -0300

    Component Header

---
+++ DTMoney/DTMoney/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from '../../components/Header/styles'
+import logo from '../../assets/logo.svg'
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logo} />
+                <NewTransactionButton>Nova transação</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
---
+++ DTMoney/DTMoney/src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+    background-color: ${props => props.theme['gray-900']};
+    padding: 2.5rem 0 7.5rem;
+`
+
+export const HeaderContent = styled.div`
+
+    max-width:1120px;
+    margin: 0 auto;
+    padding: 0 1.5rem;
+
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+`
+
+export const NewTransactionButton = styled.button`
+    display: flex;
+    padding: 12px 20px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    cursor: pointer;
+    border: none;
+    font-weight:bold;
+
+
+    border-radius: 6px;
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme['white']};
+`
