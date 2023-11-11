4412b27074d6d4ba541622109992e84b0ebd70fd
unknown <wandersonnascimento94@gmail.com>
2023-11-11 13:41:27 -0300

    Componentes visuais

--- DTMoney/DTMoney/src/components/Header/index.tsx
+++ DTMoney/DTMoney/src/components/Header/index.tsx
@@ -4,13 +4,20 @@ import {
   NewTransactionButton,
 } from '../../components/Header/styles'
 import logo from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logo} />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <img src={logo} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
