283aa79f643aba1fe1153a1adadee2fca6d604e8
unknown <wandersonnascimento94@gmail.com>
2023-11-12 20:27:44 -0300

    hooks

--- DTMoney/DTMoney/src/components/Summary/index.tsx
+++ DTMoney/DTMoney/src/components/Summary/index.tsx
@@ -1,15 +1,15 @@
-import { TransactionsContext } from '../../contexts/TransactionContext'
-import { useContext } from 'react'
 import { SummaryContainer, SummaryCard } from './styles'
 import {
   ArrowCircleUp,
   ArrowCircleDown,
   CurrencyDollar,
 } from '@phosphor-icons/react'
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-  console.log(transactions)
+  const summaryTotal = useSummary()
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -17,21 +17,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summaryTotal.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
-        <strong>R$ 1.259,00</strong>
+        <strong>{priceFormatter.format(summaryTotal.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="white" />
         </header>
-        <strong>R$ 16.141,00</strong>
+        <strong>{priceFormatter.format(summaryTotal.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
--- DTMoney/DTMoney/src/pages/Transactions/index.tsx
+++ DTMoney/DTMoney/src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionsContainer,
 } from './styled'
 import { TransactionsContext } from '../../contexts/TransactionContext'
+import { dataFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
@@ -26,11 +27,14 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dataFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
