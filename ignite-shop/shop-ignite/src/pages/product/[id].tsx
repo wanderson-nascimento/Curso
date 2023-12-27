8fad3e3aafaa9900b6dbcadee55d7317d2570494
unknown <wandersonnascimento94@gmail.com>
2023-12-27 10:33:05 -0300

    Product page CSS

--- ignite-shop/shop-ignite/src/pages/product/[id].tsx
+++ ignite-shop/shop-ignite/src/pages/product/[id].tsx
@@ -1,9 +1,21 @@
-import { useRouter } from "next/router"
+import { ImageContainer, ProductDetails, ProductPageContainer } from "@/styles/pages/product"
 
 export default function Product() {
-    const { query } = useRouter()
 
     return (
-        <h1>Product {JSON.stringify(query)}</h1>
+        <ProductPageContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camisa x</h1>
+                <span>R$ 79,90</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illum doloremque, minima dolor quasi odio earum ab in doloribus maiores eaque autem adipisci amet eum. Delectus, sint fugiat? Nobis, eligendi.</p>
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductPageContainer>
+
     )
 }
